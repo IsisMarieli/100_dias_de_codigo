4123dd0a3e313cc2e580f7cf30d968324ce1be99
IsisMarieli <Isismarieli1@gmail.com>
2025-01-07 23:38:59 -0300

    Dia 8

--- dia8/dia8.py
+++ dia8/dia8.py
@@ -1,1 +1,11 @@
-# °F = (°C x 1,8) + 32
+# °F = (°C x 9/5) + 32
+
+def c_p_f(celcius):
+    fahrenheit = (celcius * 9/5) + 32
+    return fahrenheit
+
+celc = float(input('Digite a temperatura em Celcius: '))
+
+print(f'A temperatura em Fahrenheit é: {c_p_f(celc):.2f}')
+
+c_p_f(celc)
