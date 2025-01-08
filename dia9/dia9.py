87c3b5461c1ba33a0ef6626c86cb0983bd7434ea
IsisMarieli <Isismarieli1@gmail.com>
2025-01-08 14:15:54 -0300

    Dia 9

---
+++ dia9/dia9.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+
+data = {'vendas':[100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]}
+df = pd.DataFrame(data)
+
+print(df)
+
+media = df['vendas'].mean()
+mediana = df['vendas'].median()
+desvio_padrao = df['vendas'].std()
+maximo = df['vendas'].max()
+minimo = df['vendas'].min()
+
+print(f"Média: {media}")
+print(f"Mediana: {mediana}")
+print(f"Desvio padrão: {media}")
+print(f"Maximo: {maximo}")
+print(f"Minimo: {minimo}")
