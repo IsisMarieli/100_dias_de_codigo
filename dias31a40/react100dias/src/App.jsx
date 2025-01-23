eb8726e26ac3078d327572c7880b8a0db10c0752
IsisMarieli <Isismarieli1@gmail.com>
2025-01-23 15:11:02 -0300

    Dias 31 a 40

---
+++ dias31a40/react100dias/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import MyComponent from './components/MyComponent'
+
+
+function App() {
+
+  return (
+    <div>
+      <MyComponent
+        title="Título via propriedades(props)"
+        description="Descrição via propriedades(props)"
+      />
+    </div>
+  )
+}
+
+export default App;
