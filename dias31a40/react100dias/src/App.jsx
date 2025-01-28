8cafda9b6eb3d2c0f7d427d3d6154006bc399e64
IsisMarieli <Isismarieli1@gmail.com>
2025-01-28 19:40:41 -0300

    Dias 31 a 35

--- dias31a40/react100dias/src/App.jsx
+++ dias31a40/react100dias/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import MyComponent from './components/MyComponent'
 import CondicionalComponent from './components/CondicionalComponent';
+import Counter from './components/Counter';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         description="Descrição via propriedades(props)"
       />
       <CondicionalComponent/>
+      <Counter/>
     </div>
   )
 }
